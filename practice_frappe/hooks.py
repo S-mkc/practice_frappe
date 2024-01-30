913f536fc82f92ccf95316b42093946c40302a4a
S-mkc <jaissunny009@gmail.com>
2024-01-30 13:22:20 +0545

    Stock Module modified

--- practice_frappe/hooks.py
+++ practice_frappe/hooks.py
@@ -23,7 +23,7 @@ app_license = "mit"
 # include js, css files in header of web form
 # webform_include_js = {"doctype": "public/js/doctype.js"}
 # webform_include_css = {"doctype": "public/css/doctype.css"}
-
+webform_include_css = {"custom.css": "public/css/custom.css"}
 # include js in page
 # page_js = {"page" : "public/js/file.js"}
 
