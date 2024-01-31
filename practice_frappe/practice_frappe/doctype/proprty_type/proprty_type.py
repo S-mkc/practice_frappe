efb1b4db359a55b34789f7874b07057f72d07feb
S-mkc <jaissunny009@gmail.com>
2024-01-31 12:34:36 +0545

    Property type Doctype Created

---
+++ practice_frappe/practice_frappe/doctype/proprty_type/proprty_type.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Raindrop Inc. and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class ProprtyType(Document):
+	pass
